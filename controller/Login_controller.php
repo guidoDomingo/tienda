70cb5098f622f07141f1db17ad5d26725e365fc1
Guido Ruiz <ruizbenitezguido11@gmail.com>
2024-01-10 21:31:48 -0300

    primer commit

---
+++ controller/Login_controller.php
@@ -0,0 +1,23 @@
+<?php 
+	
+	class Login
+	
+	{
+
+		public function Restaurar_Password($usuario,$contrasena){
+
+			$cmd = LoginModel::Restaurar_Password($usuario,$contrasena);
+			
+		}
+
+		public function Login_Usuario($usuario,$contrasena){
+			$url_base = 'C:\xampp743\htdocs';
+			error_log("entro en el if: " . PHP_EOL, 3, $url_base.'\tienda1\error_log.txt');
+			$cmd = LoginModel::Login_Usuario($usuario,$contrasena);
+			
+		}
+
+		
+	}
+		
+ ?>
---
+++ model/Conexion.php
@@ -0,0 +1,37 @@
+<?php
+
+	class Conexion {
+
+		public function Conectar(){
+
+
+			$driver = 'mysql'; //mysql no cambiar
+			$host = 'localhost'; //localhost
+			$dbname = 'inventario1'; //bdd
+			$username ='root'; //usuario
+			$passwd = ''; //contrase�a
+
+
+
+
+			$server=$driver.':host='.$host.';dbname='.$dbname;
+			//return var_dump($server);
+			try {
+
+				$conexion = new PDO($server,$username,$passwd);
+				//$conexion = exec("SET CHARACTER SET utf8");
+				$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+			} catch (Exception $e) {
+
+				$conexion = null;
+            	echo '<span class="label label-danger label-block">ERROR AL CONECTARSE A LA BASE DE DATOS, PRESIONE F5</span>';
+            	exit();
+			}
+
+
+			return $conexion;
+
+		}
+
+	}
