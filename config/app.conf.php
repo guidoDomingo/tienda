8abca877bff3a70438feda1effb473b806debee5
Guido Ruiz <ruizbenitezguido11@gmail.com>
2024-01-21 21:40:59 -0300

    sdfsdf

--- config/app.conf.php
+++ config/app.conf.php
@@ -1,6 +1,6 @@
 <?php
 
-    //app.conf.php es el archivo que maneja toda la aplicacion.
+    //app.conf.php es el archivo que maneja toda la aplicacion.gdfgdfg
     define("DEFAULT_VIEW","Login");  // layout vista si no ha iniciado sesion
     define("DEFAULT_LAYOUT","general.lyt.php");// default para el resto del contenido
     define("PATH_VIEW",realpath("./view/"));// carpeta de vista
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	// session_start(); estamos probando dfgdfgdfgdfdsfsdfsdffdgdfgdfg
+	// session_start(); estamos probando dfgdfgdfgdfdsfsdfsdffdgdfgdfgdfgdfg
 	 require_once("config/app.conf.php");
 	 date_default_timezone_set('America/Asuncion');
 
