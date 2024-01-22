9ad2a03aa6baa1ced341862e7e1e6b94aaad584e
Guido Ruiz <ruizbenitezguido11@gmail.com>
2024-01-21 21:35:08 -0300

    asdasds

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	// session_start(); estamos probando dfgdfgdfgdf
+	// session_start(); estamos probando dfgdfgdfgdfdsfsdfsdf
 	 require_once("config/app.conf.php");
 	 date_default_timezone_set('America/Asuncion');
 
